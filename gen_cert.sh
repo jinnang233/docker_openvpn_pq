c1277e6fe14dc37590586defac7c3ba408e32155
jinnang233 <77971207+jinnang233@users.noreply.github.com>
2025-08-30 22:48:50 +0800

    增加部分环境变量

--- gen_cert.sh
+++ gen_cert.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
+
 set -e
-
 mkdir -p /etc/openvpn
-
 cd /etc/openvpn/
 
 mkdir ccd
 touch ipp.txt
 touch openvpn-status.log
-
 openvpn --genkey --secret ta.key
 openssl genpkey -algorithm ML-DSA-87 -out ca.key
-openssl req -new -x509 -nodes -key ca.key -out ca.crt -subj "/CN=MyVPN-CA" -days 3650
-
+openssl req -new -x509 -nodes -key ca.key -out ca.crt  -subj "/CN=MyVPN-CA" -days 3650
 
 openssl genpkey -algorithm ML-DSA-87 -out server.key
 openssl req -new -key server.key -out server.csr -subj "/CN=MyVPN-Server"
 
+sed -i "s/ENV_SERVER_DNS/${ENV_SERVER_DNS}/g" /server.ext
+sed -i "s/ENV_SERVER_IP/${ENV_SERVER_IP}/g" /server.ext
 
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650 -extensions server -extfile /server.ext
+
+
+
+cp /server.ext /etc/openvpn/server.ext
 
 echo "generated: ca.key, ca.crt, server.key, server.crt"
 
--- gen_client.sh
+++ gen_client.sh
@@ -6,11 +6,11 @@ cd /etc/openvpn/clients/${folder_num}
 
 openssl genpkey -algorithm ML-DSA-87 -out /etc/openvpn/clients/${folder_num}/client.key
 openssl req -new -key /etc/openvpn/clients/${folder_num}/client.key -out /etc/openvpn/clients/${folder_num}/client.csr -subj "/CN=MyVPN-Client-${folder_num}"
-openssl x509 -req -in client.csr -CA /etc/openvpn/ca.crt -CAkey /etc/openvpn/ca.key -CAcreateserial -out /etc/openvpn/clients/${folder_num}/client.crt -days 3650 -extensions client -extfile /client.ext
+openssl x509 -req -in client.csr -CA /etc/openvpn/ca.crt -CAkey /etc/openvpn/ca.key -CAcreateserial -out /etc/openvpn/clients/${folder_num}/client.crt -extensions client -extfile /client.ext
+
 cp /etc/openvpn/ca.crt /etc/openvpn/clients/${folder_num}/ca.crt
 cp /etc/openvpn/ta.key /etc/openvpn/clients/${folder_num}/ta.key
 
 touch /etc/openvpn/ccd/MyVPN-Client-${folder_num}
 
-echo "generated in ${folder_num}:client.key, client.crt"
-
+echo "generated in ${folder_num}: client.key, client.crt"
